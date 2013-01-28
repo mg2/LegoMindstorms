10b375cb1b8ffb7dd36d24bcd6e8105f3dc32eb1
Martin Dinh <martin@dinhmail.de>
2013-01-28 13:53:04 +0100

    initial commit of project

---
+++ Curiosity/src/Main.java
@@ -0,0 +1,14 @@
+import lejos.nxt.Button;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Button.waitForAnyPress();
+	}
+
+}
