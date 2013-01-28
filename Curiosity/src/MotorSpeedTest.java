525ea074936f277a0c21a3504c074b99df68f667
Dimitar Gruev <mr.gruev@gmail.com>
2013-01-28 14:58:19 +0100

    saemtliche test dateien

---
+++ Curiosity/src/MotorSpeedTest.java
@@ -0,0 +1,31 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+
+
+public class MotorSpeedTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int speed = Motor.A.getSpeed();
+		//Motor.A.setSpeed(speed);
+		Motor.A.forward();		
+		while(!Button.ESCAPE.isDown()) {
+			if (Button.LEFT.isDown()) {
+				speed = speed - 100;
+				Motor.A.setSpeed(speed);
+				Motor.A.forward();
+			}
+			else if (Button.RIGHT.isDown()) {
+				speed = speed + 100;
+				Motor.A.setSpeed(speed);
+				Motor.A.forward();				
+			}
+			LCD.drawString(Integer.toString(Motor.A.getSpeed()), 0, 0);
+		}
+
+	}
+
+}
---
+++ Curiosity/src/MotorTest.java
@@ -0,0 +1,37 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+
+/**
+* Simplest 3 motor commands
+* @author owner.GLASSEY
+*
+*/
+public class MotorTest
+{
+/**
+* @param args
+*/
+	public static void main(String[] args)
+	{
+		//LCD.drawString("Program 1", 0, 0);
+		Motor.A.setSpeed(600);
+		while(!Button.ESCAPE.isDown()) {
+			if (Button.LEFT.isDown()) {
+				Motor.A.backward();
+				//Motor.B.backward();
+				LCD.drawString(Integer.toString(Motor.A.getSpeed()), 0, 0);
+			}
+			else if (Button.RIGHT.isDown()) {
+				Motor.A.forward();
+				//Motor.B.forward();
+				LCD.drawString(Integer.toString(Motor.A.getSpeed()), 0, 0);
+			}
+			else if (Button.ENTER.isDown()){
+				Motor.A.forward();
+				//Motor.B.backward();
+				LCD.drawString(Integer.toString(Motor.A.getSpeed()), 0, 0);
+			}
+		}
+	}
+}
