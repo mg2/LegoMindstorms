27efdf98c40c38e31bf80fb835745cdb58e1237a
Martin Dinh <martin@dinhmail.de>
2013-01-29 14:38:20 +0100

    tests for ultrasonic sensors

---
+++ Curiosity/src/UltraSensorTest.java
@@ -0,0 +1,22 @@
+import lejos.nxt.Button;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+
+
+public class UltraSensorTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		UltrasonicSensor u = new UltrasonicSensor(SensorPort.S1);
+		while(!Button.ESCAPE.isDown()) {
+			if (Button.ENTER.isDown()) {
+				System.out.println("Range value: " + u.getRange());				
+				System.out.println("Range distance: " + u.getDistance());				
+			}
+		}
+	}
+
+}
