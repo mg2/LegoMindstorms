647fba57ef2d7ca1916c181eaee8c8f040452f83
Martin Dinh <martin@dinhmail.de>
2013-01-30 10:45:21 +0100

    added static settings file

---
+++ Curiosity/src/edu/kit/curiosity/Settings.java
@@ -0,0 +1,9 @@
+package edu.kit.curiosity;
+
+public class Settings {
+
+	final static double DRIVE_SPEED = 40;
+	final static double ROTATION_SPEED = 40;
+	final static double TRACK_WIDTH = 14.5;
+	final static double WHEEL_WIDTH = 8;
+}
