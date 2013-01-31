40305c3291becabfbe7e3cded694a82337aea6a2
Jack Unseld <jackunseld@aol.com>
2013-01-31 12:23:11 +0100

    renamed Main and formatted

--- Curiosity/src/behaviorTest/Drive.java
+++ Curiosity/src/behaviorTest/Drive.java
@@ -3,27 +3,27 @@ package behaviorTest;
 import lejos.nxt.Motor;
 import lejos.robotics.subsumption.Behavior;
 
-public class Drive  implements Behavior {
-	   private boolean suppressed = false;
-	   
-	   public boolean takeControl() {
-	      return true;
-	   }
+public class Drive implements Behavior {
+	private boolean suppressed = false;
 
-	   public void suppress() {
-	      suppressed = true;
-	   }
-
-	   public void action() {
-	     suppressed = false;
-	     Motor.A.forward();
-	     Motor.C.forward();
-	     Main.i++;
-	     while( !suppressed ) {
-	    	System.out.println("drive " + Main.i);
-	        Thread.yield();
-	     }
-	     Motor.A.stop(); // clean up
-	     Motor.C.stop();
-	   }
+	public boolean takeControl() {
+		return true;
 	}
+
+	public void suppress() {
+		suppressed = true;
+	}
+
+	public void action() {
+		suppressed = false;
+		Motor.A.forward();
+		Motor.C.forward();
+		BehaviorMain.i++;
+		while (!suppressed) {
+			System.out.println("drive " + BehaviorMain.i);
+			Thread.yield();
+		}
+		Motor.A.stop(); // clean up
+		Motor.C.stop();
+	}
+}
--- Curiosity/src/behaviorTest/Hit.java
+++ Curiosity/src/behaviorTest/Hit.java
@@ -10,30 +10,28 @@ public class Hit implements Behavior {
 	private TouchSensor touch;
 	private UltrasonicSensor sonar;
 	private boolean suppressed = false;
-	
-	public Hit(SensorPort port )
-	{
-		sonar = new UltrasonicSensor( port );
+
+	public Hit(SensorPort port) {
+		sonar = new UltrasonicSensor(port);
 	}
-	
+
 	public boolean takeControl() {
 		return touch.isPressed() || sonar.getDistance() < 25;
 	}
-	
+
 	public void suppress() {
 		suppressed = true;
 	}
-	
+
 	public void action() {
 		suppressed = false;
 		Motor.A.rotate(-180, true);
 		Motor.C.rotate(-360, true);
-		Main.i++;
-		while( Motor.C.isMoving() && !suppressed ) {
-	    	System.out.println("hit " + Main.i);
+		BehaviorMain.i++;
+		while (Motor.C.isMoving() && !suppressed) {
+			System.out.println("hit " + BehaviorMain.i);
 			Thread.yield();
 		}
-		
 		Motor.A.stop();
 		Motor.C.stop();
 	}
