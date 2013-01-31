bd1b63b688f3f54625a31c6498d1a60d232bfadb
Martin Dinh <martin@dinhmail.de>
2013-01-31 19:59:01 +0100

    minor changes

--- Curiosity/src/UltraSensorTest.java
+++ Curiosity/src/UltraSensorTest.java
@@ -13,8 +13,8 @@ public class UltraSensorTest {
 		UltrasonicSensor u = new UltrasonicSensor(SensorPort.S1);
 		while(!Button.ESCAPE.isDown()) {
 			if (Button.ENTER.isDown()) {
-				System.out.println("Range value: " + u.getRange());				
-				System.out.println("Range distance: " + u.getDistance());				
+				//System.out.println("Range value: " + u.getRange());				
+				System.out.println("Distance: " + u.getDistance());				
 			}
 		}
 	}
