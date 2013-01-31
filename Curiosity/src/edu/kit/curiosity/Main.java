952958603b41c3c8dabf5fb15548171f97053408
Martin Dinh <martin@dinhmail.de>
2013-01-31 14:57:33 +0100

    updated Main to use Threads and CustomArbitrator

--- Curiosity/src/edu/kit/curiosity/Main.java
+++ Curiosity/src/edu/kit/curiosity/Main.java
@@ -3,35 +3,52 @@ package edu.kit.curiosity;
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 import lejos.nxt.Motor;
-import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import lejos.robotics.subsumption.CustomArbitrator;
 import edu.kit.curiosity.behaviors.*;
 
-public class Main {
+public class Main implements ButtonListener {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Button.ESCAPE.addButtonListener(new ButtonListener() {
-		      public void buttonPressed(Button b) {
-			        System.exit(0);
-		      }
-		      public void buttonReleased(Button b) {
-		      }
-		    });
-		
-		Motor.A.setSpeed(Motor.A.getMaxSpeed()/5);
-		Settings.PILOT.setRotateSpeed(Settings.PILOT.getMaxRotateSpeed()/5);
-		
-		Behavior b1 = new DriveForward();
-	    Behavior b2 = new WallTooClose();
-	    Behavior b3 = new WallTooFar();
-	    Behavior b4 = new HitWall();
-	    Behavior b5 = new SensorHeadPosition();
-	    Behavior b10 = new MotorAStall();
-	    Behavior [] bArray = {b1, b2, b3, b4, b5, b10};
-	    Arbitrator arby = new Arbitrator(bArray);	    
-	    arby.start();
-		}
+	// private static CustomArbitrator arbitrator;
+
+	public Main() {
+		Button.ESCAPE.addButtonListener(this);
 	}
+
+	public static void main(String[] args) throws Exception {
+		new ThreadMain();
+		Behavior b1 = new DriveForward();
+		Behavior b2 = new WallTooClose();
+		Behavior b3 = new WallTooFar();
+		Behavior b4 = new HitWall();
+		Behavior b5 = new SensorHeadPosition();
+		Behavior b10 = new MotorAStall();
+		Behavior[] bArray = { b1, b2, b3, b4, b5, b10 };
+
+		Motor.A.setSpeed(Motor.A.getMaxSpeed() / 5);
+		Settings.PILOT.setRotateSpeed(Settings.PILOT.getMaxRotateSpeed() / 5);
+
+		CustomArbitrator arbitrator = new CustomArbitrator(bArray);
+		Thread t = new Thread(arbitrator);
+		t.start();
+	}
+
+	@Override
+	public void buttonPressed(Button b) {
+		// TODO Auto-generated method stub
+		stopRunning();
+	}
+
+	@Override
+	public void buttonReleased(Button b) {
+		// TODO Auto-generated method stub
+		stopRunning();
+	}
+
+	private void stopRunning() {
+		// Stop the arbitrator, the main program and the motors.
+		System.exit(0);
+		// arbitrator.stop();
+		// resetMotors();
+	}
+}
