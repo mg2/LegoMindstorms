691e43863fe158f54b3d4fa3f318e7a1612dfc84
Martin Dinh <martin@dinhmail.de>
2013-02-01 19:50:08 +0100

    oh. accidentally deleted a variable

--- Curiosity/src/edu/kit/curiosity/Settings.java
+++ Curiosity/src/edu/kit/curiosity/Settings.java
@@ -57,6 +57,7 @@ public class Settings {
 	 */
 	public static int numberOfTurns = 0;
 	public static boolean atStartOfMaze = true;
+	public static boolean endOfMaze = false;
 	public static boolean inSwamp = false;
 	public static boolean afterSwamp = false;
 }
