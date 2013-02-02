85c0bba7c862c0187224903f67881388ad0ffd9a
Martin Dinh <martin@dinhmail.de>
2013-02-02 20:57:51 +0100

    updated main files to use correct light calibrator

--- Curiosity/src/edu/kit/curiosity/behaviors/whip/WhipMain.java
+++ Curiosity/src/edu/kit/curiosity/behaviors/whip/WhipMain.java
@@ -16,27 +16,25 @@ import edu.kit.curiosity.behaviors.SensorHeadPosition;
  */
 public class WhipMain {
 	// TODO ASUGANG!!!
-	
-	public WhipMain(){
+
+	public WhipMain() {
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		/**
 		 * Motors speed
 		 */
 		Settings.PILOT.setTravelSpeed(Settings.PILOT.getMaxTravelSpeed() * 0.40);
 		Settings.PILOT.setRotateSpeed(Settings.PILOT.getMaxRotateSpeed() * 0.15);
-		Motor.A.setSpeed(Motor.A.getMaxSpeed() / 5);		
+		Motor.A.setSpeed(Motor.A.getMaxSpeed() / 5);
 
 		Settings.motorAAngle = 90;
-		
-		
-		new LightCalibrate(true, true, false, false);
+
+		new LightCalibrate(true, true, false, false, false);
 		Settings.LIGHT.setLow(Settings.light_black);
 		Settings.LIGHT.setHigh(Settings.light_bridge + 20);
-		
-		
+
 		Behavior w0 = new DriveForward();
 		Behavior w1 = new WhipIsDown();
 		Behavior w2 = new DriveUntilWhipAbyss();
@@ -45,9 +43,9 @@ public class WhipMain {
 		Behavior w5 = new EndOfWhip();
 		Behavior w6 = new SensorHeadPosition();
 		Behavior w7 = new MotorAStall();
-		Behavior[] whipArray = {w0, w1, w2, w3, w4, w5, w6, w7};
+		Behavior[] whipArray = { w0, w1, w2, w3, w4, w5, w6, w7 };
 
-		CustomArbitrator arbitrator = new CustomArbitrator(whipArray);		
+		CustomArbitrator arbitrator = new CustomArbitrator(whipArray);
 		Thread t = new Thread(arbitrator);
 		t.start();
 	}
