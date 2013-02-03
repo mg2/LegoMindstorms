ccdb60709afe99db72553f7e5328ab081578a4e0
Martin Dinh <martin@dinhmail.de>
2013-02-03 23:32:13 +0100

    added drive backward behavior

---
+++ Curiosity/src/edu/kit/curiosity/behaviors/DriveBackward.java
@@ -0,0 +1,50 @@
+package edu.kit.curiosity.behaviors;
+
+import edu.kit.curiosity.Settings;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.*;
+
+/**
+ * This class describes the behavior to simple drive backward.
+ * 
+ * @author Curiosity
+ */
+public class DriveBackward implements Behavior {
+	private DifferentialPilot pilot;
+	private boolean suppressed = false;
+
+	/**
+	 * Constructs a new DriveForward Behavior
+	 */
+	public DriveBackward() {
+		this.pilot = Settings.PILOT;
+	}
+
+	/**
+	 * Takes always Control. returns true
+	 */
+	public boolean takeControl() {
+		return true;
+	}
+
+	/**
+	 * Initiates the cleanup when this Behavior is suppressed
+	 */
+	public void suppress() {
+		suppressed = true;
+	}
+
+	/**
+	 * Moves forward as long as this Behavior is active
+	 */
+	public void action() {
+		suppressed = false;
+		// pilot.forward();
+		while (!suppressed) {
+			// if (!pilot.isMoving()) {
+			pilot.travel(-10, true);
+			// }
+		}
+		pilot.stop();
+	}
+}
