b7eff15df6cb53473f728f573b5e127f6a894222
Jack Unseld <jackunseld@aol.com>
2013-02-03 14:07:41 +0100

    connecting and opening gate

---
+++ Curiosity/src/edu/kit/curiosity/behaviors/maze/LabyrinthGate.java
@@ -0,0 +1,64 @@
+package edu.kit.curiosity.behaviors.maze;
+
+import lejos.robotics.subsumption.Behavior;
+import edu.kit.curiosity.behaviors.bluetooth.GateCommon;
+import edu.kit.curiosity.behaviors.bluetooth.GateControl;
+import edu.kit.curiosity.behaviors.bluetooth.TurnControl;
+
+public class LabyrinthGate implements Behavior {
+
+	private boolean suppressed = false;
+	GateControl gateControl;
+
+	public LabyrinthGate() {
+		gateControl = new GateControl();
+	}
+
+	@Override
+	public boolean takeControl() {
+		return false;
+	}
+
+	@Override
+	public void action() {
+		suppressed = false;
+		while (!suppressed
+				&& !gateControl.connectionToGateSuccessful(GateCommon.GATE_1));
+		if(!suppressed) {
+			System.out.println("Connected to:"); 
+			System.out.println("	Labyrinth Gate");
+			if(gateControl.openGate()) {			
+				System.out.println("Opened Gate!");
+				if(gateControl.disconnectFromGate()) {
+					System.out.println("Disconnected...");
+				}
+			}
+		} else {
+			System.out.println("Suppressed!");
+		}
+	}
+
+	@Override
+	public void suppress() {
+		suppressed = true;
+	}
+
+	// /**
+	// * Connects to the Gate
+	// * @return true if successful else false
+	// */
+	// public boolean connect() {
+	// if(!gateControl.connectionToGateSuccessful(GateCommon.GATE_1)) {
+	// return true;
+	// }
+	// return false;
+	// }
+	/**
+	 * Disconnect from the Gate
+	 * 
+	 * @return true if successful else false
+	 */
+	public boolean disconnect() {
+		return gateControl.disconnectFromGate();
+	}
+}
