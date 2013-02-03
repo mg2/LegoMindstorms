a769e4aadf734fee8b766daea40b0e28030c8c48
Dimitar Gruev <mr.gruev@gmail.com>
2013-02-03 01:37:08 +0100

    einfache Schieber implementierung

---
+++ Curiosity/src/edu/kit/curiosity/behaviors/slider/SliderMain.java
@@ -0,0 +1,32 @@
+package edu.kit.curiosity.behaviors.slider;
+
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.robotics.subsumption.CustomArbitrator;
+import edu.kit.curiosity.Settings;
+import edu.kit.curiosity.behaviors.DriveForward;
+import edu.kit.curiosity.behaviors.MotorAStall;
+import edu.kit.curiosity.behaviors.SensorHeadPosition;
+
+public class SliderMain {
+
+
+	public static void main(String[] args) {
+
+		DifferentialPilot pilot = Settings.PILOT;
+		pilot.setAcceleration((int) (pilot.getMaxTravelSpeed() * 10));
+		pilot.setTravelSpeed(pilot.getMaxTravelSpeed());
+		Settings.motorAAngle = 90;	
+		
+		Behavior s1 = new DriveForward();
+		Behavior s2 = new SliderHit();
+		Behavior s5 = new SensorHeadPosition();
+		Behavior s6 = new MotorAStall();
+		
+		Behavior[] sliderArr = {s1, s2, s5, s6};
+		CustomArbitrator sliderArbitrator = new CustomArbitrator(sliderArr);
+		Thread t = new Thread(sliderArbitrator);
+		t.start();		
+	}
+
+}
