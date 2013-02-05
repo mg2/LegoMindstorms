cde68ca220a9661581c356c3715e7e378f9031d0
Jack Unseld <jackunseld@aol.com>
2013-02-05 12:45:30 +0100

    fixed; flags missing

--- Curiosity/src/edu/kit/curiosity/behaviors/maze/LabyrinthGate.java
+++ Curiosity/src/edu/kit/curiosity/behaviors/maze/LabyrinthGate.java
@@ -1,13 +1,20 @@
 package edu.kit.curiosity.behaviors.maze;
 
+import lejos.nxt.Button;
+import lejos.nxt.TouchSensor;
+import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Behavior;
-import edu.kit.curiosity.behaviors.bluetooth.GateCommon;
+import edu.kit.curiosity.Settings;
 import edu.kit.curiosity.behaviors.bluetooth.GateControl;
 
 public class LabyrinthGate implements Behavior {
 
 	private boolean suppressed = false;
-	GateControl gateControl;
+	private GateControl gateControl;
+	
+	private DifferentialPilot pilot = Settings.PILOT;
+	private TouchSensor touchL = Settings.TOUCH_L;
+	private TouchSensor touchR = Settings.TOUCH_R;
 
 	public LabyrinthGate() {
 		gateControl = new GateControl();
@@ -22,18 +29,21 @@ public class LabyrinthGate implements Behavior {
 	public void action() {
 		suppressed = false;
 		while (!suppressed
-				&& !gateControl.connectionToGateSuccessful(GateCommon.GATE_1));
+				&& !gateControl.connectionToGateSuccessful());
 		if(!suppressed) {
-			System.out.println("Connected to:"); 
-			System.out.println("	Labyrinth Gate");
-			if(gateControl.openGate()) {			
-				System.out.println("Opened Gate!");
-				if(gateControl.disconnectFromGate()) {
-					System.out.println("Disconnected...");
+			if(gateControl.openGate()) {
+				while(!suppressed) {
+					if(touchL.isPressed()) {
+						pilot.steer(50, -10);
+					} else if(touchR.isPressed()) {
+						pilot.steer(-50, 10);
+					}
+					pilot.steer(0, 30, true);
 				}
+				gateControl.disconnectFromGate();
 			}
 		} else {
-			System.out.println("Suppressed!");
+//			System.out.println("-!- Suppressed -!-");
 		}
 	}
 
