45cbc286a854946c4cdf3f282fe7f54d973c76c2
Tobias Hey <tobhey@gmail.com>
2013-02-06 01:09:50 +0100

    Adjusted Whip Parameters

--- Curiosity/src/edu/kit/curiosity/behaviors/whip/WhipDriveForward.java
+++ Curiosity/src/edu/kit/curiosity/behaviors/whip/WhipDriveForward.java
@@ -1,12 +1,45 @@
 package edu.kit.curiosity.behaviors.whip;
 
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Behavior;
 import edu.kit.curiosity.Settings;
 import edu.kit.curiosity.behaviors.DriveForward;
 
-public class WhipDriveForward extends DriveForward {
-	@Override
+public class WhipDriveForward implements Behavior {
+	
+	private DifferentialPilot pilot;
+	private boolean suppressed;
+
+	/**
+	 * Constructs a new DriveForward Behavior
+	 */
+	public WhipDriveForward() {
+		this.pilot = Settings.PILOT;
+	}
+	
+	/**
+	 * Initiates the cleanup when this Behavior is suppressed
+	 */
+	public void suppress() {
+		suppressed = true;
+	}
+
+	/**
+	 * Moves forward as long as this Behavior is active
+	 */
+	public void action() {
+		suppressed = false;
+		//pilot.forward();
+		while (!suppressed) {
+			//if (!pilot.isMoving()) {
+				pilot.travel(1, true);
+			//}
+		}
+		//pilot.stop();
+	}
+	
 	public boolean takeControl() {
-		return Settings.afterWhip;
+		return (Settings.beforeWhip || !Settings.afterWhip);
 	}
 
 }
