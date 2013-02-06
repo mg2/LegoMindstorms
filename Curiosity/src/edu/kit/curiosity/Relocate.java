ba21077346391ab50bfe7061fe8c4e7a36f814e3
Martin Dinh <martin@dinhmail.de>
2013-02-06 06:04:19 +0100

    missing class

---
+++ Curiosity/src/edu/kit/curiosity/Relocate.java
@@ -0,0 +1,25 @@
+package edu.kit.curiosity;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class Relocate implements Behavior {
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
